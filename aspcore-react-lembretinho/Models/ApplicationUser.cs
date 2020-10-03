f5a532c37f9d4a463e187717da9cd48e2be996d7
Rodolfo Ferreira <rodolfo_250@hotmail.com>
2020-10-03 10:53:18 -0300

    Project + solution created

---
+++ aspcore-react-lembretinho/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspcore_react_lembretinho.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
